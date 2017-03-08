5bcf64a3930ec8e7e0f87b9aec922118ab0aaf21
gaoxinge <gaoxx5@gmail.com>
2017-03-08 14:31:14 +0800

    client

---
+++ client/code/do some researches on crawler/stackoverflow/stackoverflow.py
@@ -0,0 +1,34 @@
+import requests
+from Item import Item
+from lxml import etree
+
+urls = ['http://stackoverflow.com/questions/?page=' + str(i) + '&sort=votes' for i in range(1, 3)]
+f = open('stackoverflow.txt', 'w')
+
+def http(url):
+    response = requests.get(url)
+    return response
+
+def parse(response):
+    Question = Item('Question', ['votes', 'answers', 'views', 'title', 'link'])
+    root = etree.HTML(response.text)
+    results = root.xpath('//div[@class=\'question-summary\']')
+    for result in results:
+        question = Question()
+        question['votes']   = result.xpath('div[@class=\'statscontainer\']//strong/text()')[0]
+        question['answers'] = result.xpath('div[@class=\'statscontainer\']//strong/text()')[1]
+        question['views']   = result.xpath('div[@class=\'statscontainer\']/div[@class=\'views supernova\']/text()')[0].strip()
+        question['title']   = result.xpath('div[@class=\'summary\']/h3/a/text()')[0]
+        question['link']    = result.xpath('div[@class=\'summary\']/h3/a/@href')[0]
+        yield question
+
+def store(item):
+    f.write(str(item) + '\n')
+
+while urls:
+    response = http(urls.pop(0))
+    items = parse(response)
+    for item in items:
+        store(item)
+
+f.close()
