413064f5e945fa277a8ba01457e409fa184e6062
gaoxinge <gaoxx5@gmail.com>
2017-03-19 20:47:58 +0800

    back end

--- back end/python/flask/tutorial/1/flaskr/flaskr.py
+++ back end/python/flask/tutorial/1/flaskr/flaskr.py
@@ -1,59 +1,40 @@
 import os
-import MySQLdb
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-    render_template, flash
-    
+from flask import Flask, request, session, redirect, url_for, abort, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'xxx'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:xxx@localhost/flaskr'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
 
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-    DEBUG=True,
-    SECRET_KEY='development key',
-    USERNAME='root',
-    PASSWORD='default'
-))
+class entries(db.Model):
 
-app.config.from_envvar('FLASKER_SETTINGS', slient=True)
+    id    = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40))
+    text  = db.Column(db.String(100))
+    
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
 
-def connect_db():
-    rv = sqllite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlites.ROW
-    return rv
-    
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-    
-@app.cli.command('initdb')
-def initdb_command():
-    init_db()
-    print('Initialized the database.')
-    
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-    
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-        
+db.create_all()
+
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
+    results = entries.query.all()
+    return render_template('show_entries.html', entries=results)
     
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entry (title, text) values (?, ?)',
-                [request.form['title'], request.form['text']])
-    g.db.commit()
+    entry = entries(request.form['title'], request.form['text'])
+    db.session.add(entry)
+    db.session.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
     
@@ -61,18 +42,21 @@ def add_entry():
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
+        if request.form['username'] != 'xxx':
             error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
+        elif request.form['password'] != 'xxx':
             error = 'Invalid password'
         else:
             session['logged_in'] = True
             flash('You were logged in')
             return redirect(url_for('show_entries'))
-     return render_template('login.html', error=error)
+    return render_template('login.html', error=error)
      
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
+    
+if __name__ == '__main__':
+    app.run()
