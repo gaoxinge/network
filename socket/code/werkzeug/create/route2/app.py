79b68907e769e61af0332579bec78266330827fb
gaoxinge <gaoxx5@gmail.com>
2017-03-30 11:14:44 +0800

    socket

---
+++ socket/code/werkzeug/create/route2/app.py
@@ -0,0 +1,45 @@
+import os
+from werkzeug.serving import run_simple
+from werkzeug.wrappers import Request, Response
+from werkzeug.wsgi import responder
+from werkzeug.routing import Map, Rule
+from werkzeug.exceptions import HTTPException
+from jinja2 import Environment, FileSystemLoader
+
+def render_template(template_name, **context):
+    template_path = os.path.join(os.path.dirname(__file__), 'templates')
+    jinja_env = Environment(loader=FileSystemLoader(template_path), autoescape=True)
+    t = jinja_env.get_template(template_name)
+    return Response(t.render(context), mimetype='text/html')
+
+rules = Map([])
+views = {}
+
+def get(url):
+    def wrapper(view_func):
+        rule = Rule(url, endpoint=view_func.__name__)
+        rules.add(rule)
+        views[view_func.__name__] = view_func
+        return view_func
+    return wrapper
+
+@get('/')
+def index(request):
+    return render_template('index.html')
+
+@get('/index1')
+def index1(request):
+    return render_template('index1.html')
+
+@get('/index2')
+def index2(request):
+    return render_template('index2.html')
+
+@responder
+def application(environ, start_response):
+    request = Request(environ)
+    urls = rules.bind_to_environ(request.environ)
+    return urls.dispatch(lambda e, v: views[e](request, **v),
+                         catch_http_exceptions=True)
+
+run_simple('', 5000, application)
