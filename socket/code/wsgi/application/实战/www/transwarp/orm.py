05f9507f0dfbfeedd6b23b1052c94e59b4f27e3a
gaoxinge <gaoxx5@gmail.com>
2017-04-10 08:29:50 +0800

    socket

--- socket/code/wsgi/application/实战/www/transwarp/orm.py
+++ socket/code/wsgi/application/实战/www/transwarp/orm.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Liao'
+
+'''
+Database operation module. This module is independent with web module.
+'''
+
 import time, logging
+
 import db
 
 class Field(object):
+
     _count = 0
 
     def __init__(self, **kw):
@@ -29,6 +40,7 @@ class Field(object):
         return ''.join(s)
 
 class StringField(Field):
+
     def __init__(self, **kw):
         if not 'default' in kw:
             kw['default'] = ''
@@ -37,6 +49,7 @@ class StringField(Field):
         super(StringField, self).__init__(**kw)
 
 class IntegerField(Field):
+
     def __init__(self, **kw):
         if not 'default' in kw:
             kw['default'] = 0
@@ -45,6 +58,7 @@ class IntegerField(Field):
         super(IntegerField, self).__init__(**kw)
 
 class FloatField(Field):
+
     def __init__(self, **kw):
         if not 'default' in kw:
             kw['default'] = 0.0
@@ -53,6 +67,7 @@ class FloatField(Field):
         super(FloatField, self).__init__(**kw)
 
 class BooleanField(Field):
+
     def __init__(self, **kw):
         if not 'default' in kw:
             kw['default'] = False
@@ -61,6 +76,7 @@ class BooleanField(Field):
         super(BooleanField, self).__init__(**kw)
 
 class TextField(Field):
+
     def __init__(self, **kw):
         if not 'default' in kw:
             kw['default'] = ''
@@ -69,6 +85,7 @@ class TextField(Field):
         super(TextField, self).__init__(**kw)
 
 class BlobField(Field):
+
     def __init__(self, **kw):
         if not 'default' in kw:
             kw['default'] = ''
@@ -77,7 +94,8 @@ class BlobField(Field):
         super(BlobField, self).__init__(**kw)
 
 class VersionField(Field):
-    def __init__(self, name = None):
+
+    def __init__(self, name=None):
         super(VersionField, self).__init__(name=name, default=0, ddl='bigint')
 
 _triggers = frozenset(['pre_insert', 'pre_update', 'pre_delete'])
@@ -98,15 +116,22 @@ def _gen_sql(table_name, mappings):
     return '\n'.join(sql)
 
 class ModelMetaclass(type):
+    '''
+    Metaclass for model objects.
+    '''
     def __new__(cls, name, bases, attrs):
-        if name == 'Model':
+        # skip base Model class:
+        if name=='Model':
             return type.__new__(cls, name, bases, attrs)
+
+        # store all subclasses info:
         if not hasattr(cls, 'subclasses'):
             cls.subclasses = {}
         if not name in cls.subclasses:
             cls.subclasses[name] = name
         else:
             logging.warning('Redefine class: %s' % name)
+
         logging.info('Scan ORMapping %s...' % name)
         mappings = dict()
         primary_key = None
@@ -115,6 +140,7 @@ class ModelMetaclass(type):
                 if not v.name:
                     v.name = k
                 logging.info('Found mapping: %s => %s' % (k, v))
+                # check duplicate primary key:
                 if v.primary_key:
                     if primary_key:
                         raise TypeError('Cannot define more than 1 primary key in class: %s' % name)
@@ -126,6 +152,7 @@ class ModelMetaclass(type):
                         v.nullable = False
                     primary_key = v
                 mappings[k] = v
+        # check exist of primary key:
         if not primary_key:
             raise TypeError('Primary key not defined in class: %s' % name)
         for k in mappings.iterkeys():
@@ -141,6 +168,51 @@ class ModelMetaclass(type):
         return type.__new__(cls, name, bases, attrs)
 
 class Model(dict):
+    '''
+    Base class for ORM.
+    >>> class User(Model):
+    ...     id = IntegerField(primary_key=True)
+    ...     name = StringField()
+    ...     email = StringField(updatable=False)
+    ...     passwd = StringField(default=lambda: '******')
+    ...     last_modified = FloatField()
+    ...     def pre_insert(self):
+    ...         self.last_modified = time.time()
+    >>> u = User(id=10190, name='Michael', email='orm@db.org')
+    >>> r = u.insert()
+    >>> u.email
+    'orm@db.org'
+    >>> u.passwd
+    '******'
+    >>> u.last_modified > (time.time() - 2)
+    True
+    >>> f = User.get(10190)
+    >>> f.name
+    u'Michael'
+    >>> f.email
+    u'orm@db.org'
+    >>> f.email = 'changed@db.org'
+    >>> r = f.update() # change email but email is non-updatable!
+    >>> len(User.find_all())
+    1
+    >>> g = User.get(10190)
+    >>> g.email
+    u'orm@db.org'
+    >>> r = g.delete()
+    >>> len(db.select('select * from user where id=10190'))
+    0
+    >>> import json
+    >>> print User().__sql__()
+    -- generating SQL for user:
+    create table `user` (
+      `id` bigint not null,
+      `name` varchar(255) not null,
+      `email` varchar(255) not null,
+      `passwd` varchar(255) not null,
+      `last_modified` real not null,
+      primary key(`id`)
+    );
+    '''
     __metaclass__ = ModelMetaclass
 
     def __init__(self, **kw):
@@ -157,30 +229,49 @@ class Model(dict):
 
     @classmethod
     def get(cls, pk):
+        '''
+        Get by primary key.
+        '''
         d = db.select_one('select * from %s where %s=?' % (cls.__table__, cls.__primary_key__.name), pk)
         return cls(**d) if d else None
 
     @classmethod
     def find_first(cls, where, *args):
+        '''
+        Find by where clause and return one result. If multiple results found, 
+        only the first one returned. If no result found, return None.
+        '''
         d = db.select_one('select * from %s %s' % (cls.__table__, where), *args)
         return cls(**d) if d else None
 
     @classmethod
     def find_all(cls, *args):
+        '''
+        Find all and return list.
+        '''
         L = db.select('select * from `%s`' % cls.__table__)
         return [cls(**d) for d in L]
 
     @classmethod
     def find_by(cls, where, *args):
+        '''
+        Find by where clause and return list.
+        '''
         L = db.select('select * from `%s` %s' % (cls.__table__, where), *args)
         return [cls(**d) for d in L]
 
     @classmethod
     def count_all(cls):
+        '''
+        Find by 'select count(pk) from table' and return integer.
+        '''
         return db.select_int('select count(`%s`) from `%s`' % (cls.__primary_key__.name, cls.__table__))
 
     @classmethod
     def count_by(cls, where, *args):
+        '''
+        Find by 'select count(pk) from table where ... ' and return int.
+        '''
         return db.select_int('select count(`%s`) from `%s` %s' % (cls.__primary_key__.name, cls.__table__, where), *args)
 
     def update(self):
